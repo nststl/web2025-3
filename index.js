88b9844f6be755ab2c9fbeec986e600fd5afafe8
Anastasiia Katelo web2025-3 <anastasiakatelo@gmail.com>
2025-04-17 12:30:36 +0300

    add fix code

--- index.js
+++ index.js
@@ -9,41 +9,40 @@ program
 
 const options = program.opts();
 
-if (!options.input) 
-{
+if (!options.input) {
     console.error('Please, specify input file');
     process.exit(1);
 }
 
-if (!fs.existsSync(options.input)) 
-{
+if (!fs.existsSync(options.input)) {
     console.error('Cannot find input file');
     process.exit(1);
 }
 
-try
-{
+try {
     const data = fs.readFileSync(options.input, 'utf8');
     const parsedData = JSON.parse(data);
   
-    const filteredData = parsedData.filter(item => item.parent === 'BS3_BanksLiab');
+    const filteredData = parsedData.filter(function(item) {
+        return item.parent === 'BS3_BanksLiab';
+    });
 
-    const formattedData = filteredData.map(item => `${item.txten}:${item.value}`);
+    const formattedData = filteredData.map(function(item) {
+        return item.txten + ':' + item.value;
+    });
 
-    if (options.output) 
-    {
+    if (options.output) {
         const outputResult = formattedData.join('\n');
         fs.writeFileSync(options.output, outputResult);
     }
   
-    if (options.display) 
-    {
-        formattedData.forEach(item => console.log(item));
+    if (options.display) {
+        formattedData.forEach(function(item) {
+            console.log(item);
+        });
     }
   
-}
-catch (error) 
-{
+} catch (error) {
     console.error('Error processing file:', error.message);
     process.exit(1);
 }
